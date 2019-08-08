a6578a41e388d498c830a9f1bdca556d83088a5d
Vicente Santiago <vicentejsp@outlook.com>
2019-08-08 04:59:13 -0500

    Primera implementacion de Formularios reactivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reactiveForm';
+  title = 'Formulario Reactivo';
+  formulario: FormGroup;
+
+  constructor( private formBilder: FormBuilder) {
+    this.formulario = this.formBilder.group({
+      nombre:[null, Validators.required],
+      email:[null, Validators.compose([Validators.email, Validators.required])],
+      pwd:[null, Validators.compose([Validators.minLength(4), Validators.required])]
+    });
+  }
+
+  enviar(formulario: FormGroup){
+    console.log('Leyendo datos');
+    console.log(formulario.value);
+  }
+
 }
